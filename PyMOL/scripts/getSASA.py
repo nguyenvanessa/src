7d3f54a8cbc98ef15b7561ccd17e799b9ba093d6
Vanessa Nguyen <46462831+nguyenvanessa@users.noreply.github.com>
2020-04-20 15:39:34 -0700

    fix file write location in getSASA

--- PyMOL/scripts/getSASA.py
+++ PyMOL/scripts/getSASA.py
@@ -5,19 +5,25 @@ from pymol import cmd, stored
 pymol.finish_launching()
 
 cmd.set('dot_solvent', 1)
-cmd.set('dot_density', 4)
+cmd.set('dot_density', 4)   # Lowest resolution
 
-cmd.load('../pdbs/2src_human_numbering.pdb')  # use the name of your pdb file
+pdb = '1y57_human_numbering'
+
+cmd.load('../pdbs/' + pdb + '.pdb')  # use the name of your pdb file
 stored.residues = []
 cmd.iterate('name ca', 'stored.residues.append(resi)')
 
 sasa_per_residue = []
+idx = []
 for i in stored.residues:
     sasa_per_residue.append(cmd.get_area('resi %s' % i))
+    idx.append(i)
 
-with open('2src_SASA.txt', 'w') as f:
-    for item in sasa_per_residue:
-        f.write("%s\n" % item)
+with open(pdb + '_SASA.txt', 'w') as f:
+    for i, item in zip(idx, sasa_per_residue):
+        #f.write("%s\n" % item)
+        line = "{}\t{}\n".format(i, item)
+        f.write(line)
 
 print sum(sasa_per_residue)
 print cmd.get_area('all')  # just to check that the sum of sasa per residue equals the total area
